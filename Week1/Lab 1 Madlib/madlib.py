f7472502344c6cbf7ac32c6f822c88be5c072496
sgehrke <shaungehrke@shaungerkesimac.eau.wi.charter.com>
2014-09-03 22:06:28 -0500

    Created 3 strings
    First three prompts to be stored in a dict() object and then ran
    through by a for loop

---
+++ Week1/Lab 1 Madlib/madlib.py
@@ -0,0 +1,39 @@
+'''
+Name: Shaun Gehrke
+Class: Design Patterns fro Web Programming 1409
+Assignment: Python MadLibs
+
+'''
+# Prompt the user for 3 strings
+# One Dictionary - use to store prompted items like an object
+
+stringNames = dict()
+stringNames = {
+    "user": raw_input("Please enter your name: "),
+    "user_noun": raw_input("Enter a creative noun: "),
+    "user_adjective": raw_input("Enter a descriptive word: ")
+
+    }
+
+for i in stringNames:
+
+    print stringNames[i]
+
+
+
+
+# Prompt the user for 3 integers
+
+
+
+# One Array to store the information
+
+# One Dictionary - like an object
+
+# 2 mathematical operators
+
+# 2 Conditional statements (if)
+
+# 1 Function that must return a value and have 2 parameters
+
+# 1 Loop for or while
