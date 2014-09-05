22632a6b6f9b29c208478862c0e5414c7f07e54b
sgehrke <shaungehrke@shauns-mbp.eau.wi.charter.com>
2014-09-04 21:23:22 -0500

    Adjust array to be usable
    The array wasn’t being used in the messege just the variables so -
    update

--- Week1/Lab 1 Madlib/madlib.py
+++ Week1/Lab 1 Madlib/madlib.py
@@ -10,56 +10,53 @@ Assignment: Python MadLib
 teamString = dict()
 teamString = {
     "user_team": raw_input("Please enter your favorite NFL teams name: "),
-    "user_verb": raw_input("Enter a verb: "),
+    "user_place": raw_input("Enter a place: "),
     "user_adjective": raw_input("Enter a descriptive word: ")
     }
 
-# 1 Loop for or while
+# 1 Loop for
 
 
 
 # Getting the responses and storing them in variables
-fav_team = teamString["user_team"]
-word = teamString["user_verb"]
+fav_team = teamString["user_team"].upper()
+word = teamString["user_place"]
 adjective = teamString["user_adjective"]
+
+if fav_team not "PACKERS":
+    winner = "Green Bay Packers"
+
 
 # One Array to store the information
 user_int = []
 
 # Prompt the user for 3 integers
-score_win = raw_input("Enter a number above 14: ")
-score_lose = raw_input("Enter a number under 14: ")
-year_int = raw_input("Enter a number above 2015: ")
-
-user_int.append("score_win")
-user_int.append("score_lose")
-user_int.append("year_int")
-#print user_int
+score_win = user_int.append(raw_input("Enter a number above 14: "))
+score_lose = user_int.append(raw_input("Enter a number under 14: "))
+year_int = user_int.append(raw_input("Enter a number above 2015: "))
 
 
+print user_int
 
-# 2 mathematical operators
+if user_int
 
-
-
-
-winner = "Green Bay Packers"
-
+# 1 more mathematical operators
 
 
 top_teams = ["Broncos ", "49ers ", "Eagles ", "Patriots", "Colts ", "Saints ", "Falcons ", "Chiefs"]
 length_top_teams = len(top_teams)
 tonights_teams = ["Packers", "Seahawks"]
 length_tonights_teams = len(tonights_teams)
-
 # use a randomizor to pick the winner of tonights game
 # 1 Function that must return a value and have 2 parameters
 # this randomizer will dynamically return things
 def randomizer(b,c):
     import random
+    b -= 1 # to start the index from 0 and is one mathematical operator b = b -1
     a = random.randint(0,b)
+    print b
     print "the random number is", str(a)
-    return c[a]
+    return c[a].upper()
 
 
 random_team = randomizer(length_top_teams, top_teams)
@@ -78,26 +75,36 @@ print tonights_pick
 
 # 2 Conditional statements (if)
 # if not the Packers or the Seahawks print message_1
-if fav_team == "Packers" or fav_team  == "Seahawks":
-    message_1 = '''
-        The 2014 NFL season kicks off tonight with the Green Bay Packers taking on the Seattle Seahawks. The
-        {tonights_pick} will win this game and go on to play in the Superbowl. If your {fav_team} are fortunate enough
-        make it to the Superbowl, they will be defeated by the {winner} by a score of {score_win} - {score_lose}. The
-        next time they will have an opportunity will be in {year_int}.
-    '''
-    message_1 = message_1.format(**locals())
-    print message_1
+# if fav_team == "PACKERS" and tonights_pick == "PACKERS":
+message_1 = '''
+    The 2014 NFL season kicks off tonight with the Green Bay Packers taking on the Seattle Seahawks at {word}. The
+    {tonights_pick} will win this game and go on to be {adjective}. The {fav_team} will play the {random_team}
+    and win by a score of {user_int[0]} - {user_int[1]}. The next time they will have an opportunity to play in a
+    Superbowl will be in {user_int[2]}.
+'''
+message_1 = message_1.format(**locals())
+print message_1
 
-# if the Packers print message_2
-message_2 = '''
-
-    '''
-print message_2
-
-# if the Seahawks print message_3
-message_3 = '''
-
-    '''
-print message_3
+# elif fav_team == "SEAHAWKS":
+#     message_2 = '''
+#         The 2014 NFL season kicks off tonight with the Green Bay Packers taking on the Seattle Seahawks at (place). The
+#         {tonights_pick} will win this game and go on to in the Superbowl. If your {fav_team} are fortunate enough
+#         make it to the Superbowl, they will play {random_team} and win/lose by a score of {user_int[1]} - {score_lose}. The
+#         next time they will have an opportunity to play in a Superbowl will be in {year_int}.
+#     '''
+#     message_2 = message_2.format(**locals())
+#     print message_2
+#
+# # else the Packers print message_2
+# message_2 = '''
+#
+#     '''
+# print message_2
+#
+# # if the Seahawks print message_3
+# message_3 = '''
+#
+#     '''
+# print message_3
 
 
