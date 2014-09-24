2b4051ad2937dcabf1df0f334b3f654a3cb75628
sgehrke <shaungehrke@Shaun-Gehrkes-iMac.local>
2014-09-24 16:37:00 -0500

    Model/ contractor class

--- Application with API/api/main.py
+++ Application with API/api/main.py
@@ -13,6 +13,16 @@ import json # makes json objects available
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+# Model class created as a constructor for the data
+class ModelClass()
+    def __init__(self):
+        self.title = ''
+        self.year = ''
+        self.rating = ''
+        self.synopsis = ''
+        self.thumbnail = ''
+        self.cast = []
+        self.char = []
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
